0260c26edbcd91b84a641aef2e450d6daaac8c43
bopaf <116653905+bopaf@users.noreply.github.com>
2023-03-28 00:06:51 +0200

    added basic displaying posts functionality

--- flaskBlog.py
+++ flaskBlog.py
@@ -1,9 +1,23 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts =[
+{
+   'author': 'pablo escobar',
+   'title': "first post",
+   'content': 'i love italian cars',
+   'post_date': '27/03/2023'
+},
+{
+   'author': 'george clooney',
+   'title': "second post",
+   'content': 'i love fixing pablo\'s cars',
+   'post_date': '28/03/2023'
+}
+]
 @app.route('/')
 def home_window():
-   return render_template('home.html')
+   return render_template('home.html', posts = posts)
 
 @app.route('/about')
 def about_app():
